9e0a6f7d11f87cbe9afc7ad9894f59029dd07794
Anupam raj <75802441+anupamraj2001@users.noreply.github.com>
2023-01-15 12:36:57 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
+        'host': 'agro-bazar.c7881x5mkzin.ap-south-1.rds.amazonaws.com',
         'user': 'root',
         'password': '123456789'
     },
